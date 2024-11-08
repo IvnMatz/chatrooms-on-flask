d52d274d152f5883b4e00a57789fa6fe370dede4
IvnMatz <ivanholaxderespaldo@gmail.com>
2024-11-07 20:55:15 -0600

    socketio local

--- main.py
+++ main.py
@@ -88,5 +88,5 @@ def handle_message(msg):
     send(message, broadcast=True)
 
 if __name__ == '__main__':
-    #socketio.run(app, host="0.0.0.0", port=5000)
-    socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000)
+    #socketio.run(app, debug=True)
